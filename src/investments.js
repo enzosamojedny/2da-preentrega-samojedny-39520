a2c306f03b9530150ac53b9ce8555e75a4c91d95
Enzo Samojedny <enzosamojedny@gmail.com>
2023-05-18 19:42:15 -0300

    IBM ticker added

--- src/investments.js
+++ src/investments.js
@@ -1,20 +1,15 @@
-const request = require('request');
+
+const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=IBM&apikey=0Y9J544VII9BYP7K';
+
 
 // https://www.alphavantage.co/support/#api-key
-const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=0Y9J544VII9BYP7K';
+const symbol = 'IBM'
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        const timeSeries = data['Weekly Adjusted Time Series'];
+        const lastDate = Object.keys(timeSeries)[0];
+        const lastPrice = timeSeries[lastDate]['5. adjusted close'];
+        alert(`The last adjusted close price for ${symbol} is: $${lastPrice}`);
+    });
 
-request.get({
-    url: url,
-    json: true,
-    headers: { 'User-Agent': 'request' }
-}, (err, res, data) => {
-    if (err) {
-        console.log('Error:', err);
-    } else if (res.statusCode !== 200) {
-        console.log('Status:', res.statusCode);
-    } else {
-        // data is successfully parsed as a JSON object:
-        console.log(data);
-    }
-});
-alpha.data.weekly(IBM, outputsize, datatype, interval);
