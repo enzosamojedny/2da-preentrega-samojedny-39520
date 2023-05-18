37c4a5777b0631f03de453b795163390f8aa4bf2
Enzo Samojedny <enzosamojedny@gmail.com>
2023-05-18 18:42:59 -0300

    alphaVantage API added

---
+++ src/investments.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+// https://www.alphavantage.co/support/#api-key
+const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=0Y9J544VII9BYP7K';
+
+request.get({
+    url: url,
+    json: true,
+    headers: { 'User-Agent': 'request' }
+}, (err, res, data) => {
+    if (err) {
+        console.log('Error:', err);
+    } else if (res.statusCode !== 200) {
+        console.log('Status:', res.statusCode);
+    } else {
+        // data is successfully parsed as a JSON object:
+        console.log(data);
+    }
+});
