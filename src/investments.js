99e625b883af604557c0680a46b5f4683f8fa955
Enzo Samojedny <enzosamojedny@gmail.com>
2023-05-19 14:20:05 -0300

    API request update

--- src/investments.js
+++ src/investments.js
@@ -1,13 +1,8 @@
-
-
-const urlMonthly = 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=IBM&apikey=0Y9J544VII9BYP7K'
-const marketSentiment = 'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&apikey=0Y9J544VII9BYP7'
 const urlIBM = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=IBM&apikey=0Y9J544VII9BYP7K';
 const urlMSFT = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=MSFT&apikey=0Y9J544VII9BYP7K';
 const urlGOOGL = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=GOOGL&apikey=0Y9J544VII9BYP7K';
 const urlNVDA = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=NVDA&apikey=0Y9J544VII9BYP7K';
 const urlWMT = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=WMT&apikey=0Y9J544VII9BYP7K';
-const urlBABA = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=BABA&apikey=0Y9J544VII9BYP7K';
 
 fetch(urlIBM)
     .then((response) => response.json())
@@ -19,7 +14,7 @@ fetch(urlIBM)
         const lastVolume = timeSeries[lastDate]['6. volume'];
         const lastPrice = timeSeries[lastDate]['5. adjusted close'];
         //IBM
-        const IBMID = document.getElementById('IBM-ID')
+        const IBMID = document.getElementById('IBM-OPEN')
         let IBMp = document.createElement('p')
         IBMp.className = 'IBMp-JS'
         IBMp.textContent = lastOpen
@@ -35,7 +30,7 @@ fetch(urlMSFT)
         const lastVolume = timeSeries[lastDate]['6. volume'];
         const lastPrice = timeSeries[lastDate]['5. adjusted close'];
         //MSFT
-        const MSFTID = document.getElementById('MSFT-ID')
+        const MSFTID = document.getElementById('MSFT-OPEN')
         let MSFTp = document.createElement('p')
         MSFTp.className = 'MSFTp-JS'
         MSFTp.textContent = lastOpen
@@ -50,7 +45,7 @@ fetch(urlGOOGL)
         const lastVolume = timeSeries[lastDate]['6. volume'];
         const lastPrice = timeSeries[lastDate]['5. adjusted close'];
         //GOOGL
-        const GOOGLID = document.getElementById('GOOGL-ID')
+        const GOOGLID = document.getElementById('GOOGL-OPEN')
         let GOOGLp = document.createElement('p')
         GOOGLp.className = 'GOOGLp-JS'
         GOOGLp.textContent = lastOpen
@@ -65,7 +60,7 @@ fetch(urlNVDA)
         const lastVolume = timeSeries[lastDate]['6. volume'];
         const lastPrice = timeSeries[lastDate]['5. adjusted close'];
         //NVDA
-        const NVDAID = document.getElementById('NVDA-ID')
+        const NVDAID = document.getElementById('NVDA-OPEN')
         let NVDAp = document.createElement('p')
         NVDAp.className = 'NVDAp-JS'
         NVDAp.textContent = lastOpen
@@ -79,25 +74,22 @@ fetch(urlWMT)
         const lastOpen = timeSeries[lastDate]['1. open'];
         const lastVolume = timeSeries[lastDate]['6. volume'];
         const lastPrice = timeSeries[lastDate]['5. adjusted close'];
-        //WMT
-        const WMTID = document.getElementById('WMT-ID')
+        //WMT OPEN
+        const WMTID = document.getElementById('WMT-OPEN')
         let WMTp = document.createElement('p')
         WMTp.className = 'WMTp-JS'
         WMTp.textContent = lastOpen
         WMTID.appendChild(WMTp)
-    });
-fetch(urlBABA)
-    .then((response) => response.json())
-    .then((data) => {
-        const timeSeries = data['Weekly Adjusted Time Series'];
-        const lastDate = Object.keys(timeSeries)[0];
-        const lastOpen = timeSeries[lastDate]['1. open'];
-        const lastVolume = timeSeries[lastDate]['6. volume'];
-        const lastPrice = timeSeries[lastDate]['5. adjusted close'];
-        //BABA
-        const BABAID = document.getElementById('BABA-ID')
-        let BABAp = document.createElement('p')
-        BABAp.className = 'BABAp-JS'
-        BABAp.textContent = lastOpen
-        BABAID.appendChild(BABAp)
+        //WMT OPEN
+        const WMTCLOSE = document.getElementById('WMT-CLOSE')
+        let WMTclose = document.createElement('p')
+        WMTclose.className = 'WMTp-JS'
+        WMTclose.textContent = lastClose
+        WMTCLOSE.appendChild(WMTp)
+        //WMT OPEN
+        const WMTVOL = document.getElementById('WMT-VOL')
+        let WMTvol = document.createElement('p')
+        WMTvol.className = 'WMTvol-JS'
+        WMTvol.textContent = lastVolume
+        WMTVOL.appendChild(WMTvol)
     });
