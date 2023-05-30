4934f186834a742cb5da03134160db32962e75c5
Enzo Samojedny <enzosamojedny@gmail.com>
2023-05-29 22:31:37 -0300

    small updates

--- src/transfers.js
+++ src/transfers.js
@@ -1,29 +1,40 @@
 import { CuentaCorriente, Cliente, Plazo_Fijo } from './classes.js';
 import { userData } from './transfer2.js';
 const exportedVariables = userData()
-let saldoCuenta = exportedVariables.cuentaDeCliente.verSaldo()
-let cuentaTransferenciaCliente = exportedVariables.cuentaDeCliente
 const date = moment().format('LLL');
 const cliente1 = new Cliente("Pablo Lescano", "38.112.194");
 const cliente2 = new Cliente("Laura Gomez", "23.456.321");
-const cuentaDePablo = new CuentaCorriente(cliente1, "Santander", "001", 0);
-const cuentaDeLaura = new CuentaCorriente(cliente2, "Brubank", "002", 0);
-const divTransferCheck = document.querySelector('.div-laurita')
+const cuentaDePablo = new CuentaCorriente(cliente1, "Santander", 0, "001");
+const cuentaDeLaura = new CuentaCorriente(cliente2, "Brubank", 0, "002");
 const h6DisplayData = document.querySelector('.cuenta-cliente')
-const mainSelect = document.getElementById('main-select')
+let mainSelect = document.querySelector('select');
 const transferButton = document.querySelector('.transfer-button')
 const saldoClienteBtn = document.getElementById('saldo-cliente-btn')
-h6DisplayData.textContent = "Cuenta Cliente " + exportedVariables.nombreCliente
 
-let transferFunction = saldoClienteBtn.addEventListener('click', function () {
-    //el saldo no se actualiza porque siempre retoma los 10000 de saldo 
+h6DisplayData.textContent = "Cuenta Cliente " + exportedVariables.nombreCliente
+function verElSaldo() {
     let saldoClienteP = document.getElementById('saldo-cliente-p')
     let saldoCliente = exportedVariables.cuentaDeCliente.verSaldo()
     saldoClienteP.textContent = "Saldo: " + saldoCliente;
-})
-transferFunction
+}
+saldoClienteBtn.addEventListener('click', verElSaldo)
+
+mainSelect.addEventListener('change', function () {
+    console.log(mainSelect.value);
+});
 
 transferButton.addEventListener('click', function () {
+    if (mainSelect.value === 'Pablo Lescano') {
+        transferButton.id = 'transferir-pablo-btn'
+        let transferCustomer1 = document.querySelector('#transferir-pablo-btn')
+        transferCustomer1.addEventListener("click", transferCustomer);
+    } else if (mainSelect.value === 'Laura Gomez') {
+        transferButton.id = 'transferir-laura-btn'
+        let transferCustomer2 = document.querySelector('#transferir-laura-btn')
+        transferCustomer2.addEventListener("click", transfLaura);
+    } else {
+        console.log("error here")
+    }
     function randomize() {
         const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
         const arrOp = [9, 8, 7, 6, 5, 4, 3, 2, 1];
@@ -35,22 +46,13 @@ transferButton.addEventListener('click', function () {
             password += randomNum;
         }
         return password;
-    } let generatedPassword = randomize();
-    if (mainSelect.value === 'Pablo Lescano') {
-        transferButton.id = 'transferir-pablo-btn'
-        let transferCustomer1 = document.querySelector('#transferir-pablo-btn')
-        transferCustomer1.addEventListener("click", transferCustomer);
-    } else if (mainSelect.value === 'Laura Gomez') {
-        transferButton.id = 'transferir-laura-btn'
-        let transferCustomer2 = document.querySelector('#transferir-laura-btn')
-        transferCustomer2.addEventListener("click", transfLaura);
-    }
+    } const generatedPassword = randomize();
     function transferCustomer() {
         let inputCustomer = document.getElementById('input-cliente-final').value;
         let transferirCant = parseFloat(inputCustomer)
-        if (transferirCant <= saldoCuenta) {
+        if (transferirCant <= exportedVariables.cuentaDeCliente.verSaldo()) {
             Swal.fire({
-                title: 'You are about to send $' + inputCustomer + ' to ' + cliente1.nombreCliente,
+                title: 'You are about to send $' + inputCustomer + ' to ' + cliente2.nombreCliente,
                 text: "You won't be able to revert this!",
                 icon: 'warning',
                 showCancelButton: true,
@@ -59,57 +61,64 @@ transferButton.addEventListener('click', function () {
                 confirmButtonText: 'Proceed with transfer'
             }).then((result) => {
                 if (result.isConfirmed) {
-                    cuentaTransferenciaCliente.transferencia(transferirCant, cuentaDePablo);
+                    cuentaDePablo.transferencia(transferirCant, cuentaDeLaura);
                     Swal.fire(
                         'Transfer sent to recipient',
                         '',
                         'success'
                     )
+                    //PROBLEM NOT HERE//PROBLEM NOT HERE//PROBLEM NOT HERE//PROBLEM NOT HERE
+                    let div = document.createElement('div')
+                    div.id = 'resume';
+                    div.className = 'resume-container';
+                    let h2 = document.createElement('h2')
+                    h2.textContent = "Resumen de Transferencia"
+                    div.appendChild(h2)
+                    let pnumerotransaccion = document.createElement('p')
+                    pnumerotransaccion.textContent = "Destinatario: " + cliente2.nombreCliente
+                    div.appendChild(pnumerotransaccion)
+                    let p = document.createElement('p')
+                    p.className = 'p-js'
+                    p.textContent = "Origen: " + exportedVariables.nombreCliente
+                    div.appendChild(p)
+                    let ptimePablo = document.createElement('p')
+                    ptimePablo.textContent = "Fecha de transferencia: " + date;
+                    div.appendChild(ptimePablo)
+                    let pdestiny = document.createElement('p')
+                    pdestiny.textContent = "Número de operación: " + generatedPassword
+                    div.appendChild(pdestiny)
+                    let p3 = document.createElement('p')
+                    p3.textContent = "Banco " + cuentaDeLaura.banco
+                    div.appendChild(p3)
+                    let p4 = document.createElement('p')
+                    p4.textContent = "Cuenta " + cuentaDeLaura.numeroCuenta
+                    div.appendChild(p4)
+                    let p5 = document.createElement('p')
+                    p5.textContent = "DNI " + cliente1.dniCliente
+                    div.appendChild(p5)
+                    let p6 = document.createElement('p')
+                    div.appendChild(p6)
+                    document.body.appendChild(div)
+                    let p7 = document.querySelector('#resume p:last-child')
+                    p7.textContent = "Monto: $" + transferirCant;
+                    let resumeContainer = document.getElementById('resume');
+                    resumeContainer.style.display = 'block';
                 }
             });
-            let div = document.createElement('div')
-            div.id = 'resume';
-            div.className = 'resume-container';
-            let h2 = document.createElement('h2')
-            h2.textContent = "Resumen de Transferencia"
-            div.appendChild(h2)
-            let pnumerotransaccion = document.createElement('p')
-            pnumerotransaccion.textContent = "Destinatario: " + cliente1.nombreCliente
-            div.appendChild(pnumerotransaccion)
-            let p = document.createElement('p')
-            p.className = 'p-js'
-            p.textContent = "Origen: " + exportedVariables.clienteUsuario.nombreCliente
-            div.appendChild(p)
-            let ptimePablo = document.createElement('p')
-            ptimePablo.textContent = "Fecha de transferencia: " + date;
-            div.appendChild(ptimePablo)
-            let pdestiny = document.createElement('p')
-            pdestiny.textContent = "Número de operación: " + generatedPassword
-            div.appendChild(pdestiny)
-            let p3 = document.createElement('p')
-            p3.textContent = "Banco " + cuentaDeLaura.banco
-            div.appendChild(p3)
-            let p4 = document.createElement('p')
-            p4.textContent = "Cuenta " + cuentaDeLaura.numeroCuenta
-            div.appendChild(p4)
-            let p5 = document.createElement('p')
-            p5.textContent = "DNI " + cliente1.dniCliente
-            div.appendChild(p5)
-            let p6 = document.createElement('p')
-            div.appendChild(p6)
-            document.body.appendChild(div)
-            let p7 = document.querySelector('#resume p:last-child')
-            p7.textContent = "Monto: $" + transferirCant;
-            let resumeContainer = document.getElementById('resume');
-            resumeContainer.style.display = 'block';
+        } else {//PROBLEM NOT HERE
+            Swal.fire({
+                icon: 'error',
+                title: 'Insufficient funds in your savings account',
+                text: '',
+            })
         }
     }
     function transfLaura() {
         let inputLaura = document.getElementById('input-cliente-final').value;
         let transferirCantidad = parseFloat(inputLaura);
-        if (transferirCantidad <= exportedVariables.cuentaDeCliente.verSaldo()) {//why isnt this accepting the name of variable
+        if (transferirCantidad <= exportedVariables.cuentaDeCliente.verSaldo()) {
             Swal.fire({
-                title: 'You will send $' + inputLaura + ' to ' + cliente2.nombreCliente,
+                title: 'You will send $' + inputLaura + ' to ' + cliente1.nombreCliente,
                 text: "You won't be able to revert this!",
                 icon: 'warning',
                 showCancelButton: true,
@@ -118,13 +127,13 @@ transferButton.addEventListener('click', function () {
                 confirmButtonText: 'Proceed with transfer'
             }).then((result) => {
                 if (result.isConfirmed) {
-                    cuentaTransferenciaCliente.transferencia(transferirCantidad, cuentaDeLaura);
+                    cuentaDeLaura.transferencia(transferirCantidad, cuentaDePablo);
                     Swal.fire(
                         'Transfer sent to recipient',
                         '',
                         'success'
                     )
-                    // LAURA resumen de transferencia
+                    //PROBLEM NOT HERE//PROBLEM NOT HERE//PROBLEM NOT HERE//PROBLEM NOT HERE
                     let divLaura = document.createElement('div');
                     divLaura.id = 'resumeLaura';
                     divLaura.className = 'resume-container';
@@ -132,11 +141,11 @@ transferButton.addEventListener('click', function () {
                     h2Laura.textContent = "Resumen de Transferencia";
                     divLaura.appendChild(h2Laura);
                     let pnumerotransaccionlaura = document.createElement('p');
-                    pnumerotransaccionlaura.textContent = "Destinatario: " + cliente2.nombreCliente;
+                    pnumerotransaccionlaura.textContent = "Destinatario: " + cliente1.nombreCliente;
                     divLaura.appendChild(pnumerotransaccionlaura);
                     let pLaura = document.createElement('p');
                     pLaura.className = 'p-js-laura';
-                    pLaura.textContent = "Origen: " + exportedVariables.clienteUsuario.nombreCliente;
+                    pLaura.textContent = "Origen: " + exportedVariables.nombreCliente
                     divLaura.appendChild(pLaura);
                     let ptime = document.createElement('p')
                     ptime.textContent = "Fecha de transferencia: " + date;
@@ -161,17 +170,12 @@ transferButton.addEventListener('click', function () {
                     let resumeContainerLaura = document.getElementById('resumeLaura');
                     resumeContainerLaura.style.display = 'block';
                 }
+                exportedVariables.cuentaDeCliente.verSaldo()
             })
-        } else if (transferirCantidad > exportedVariables.cuentaDeCliente.verSaldo()) {
+        } else {//PROBLEM NOT HERE
             Swal.fire({
                 icon: 'error',
-                title: 'Insufficient funds in your account',
-                text: '',
-            })
-        } else {
-            Swal.fire({
-                icon: 'error',
-                title: 'Please enter numeric input',
+                title: 'Insufficient funds in your savings account',
                 text: '',
             })
         }
@@ -179,124 +183,3 @@ transferButton.addEventListener('click', function () {
     transferCustomer()
     transfLaura()
 })
-
-
-
-// function eliminarResumenTransferPablo() {
-//     setTimeout(function () {
-//         let resumePablo = document.getElementById('resume');
-//         if (resumePablo) {
-//             resumePablo.remove();
-//         }
-//     }, 15000);
-// }
-// eliminarResumenTransferPablo()
-
-// function eliminarResumenTransferLaura() {
-//     setTimeout(function () {
-//         let resumeLaura = document.getElementById('resumeLaura');
-
-//         if (resumeLaura) {
-//             resumeLaura.remove();
-//         }
-//     }, 15000);
-// }
-// eliminarResumenTransferLaura()
-
-// const pfPabloBtn = document.getElementById("pf-pablo");
-// const pfLauraBtn = document.getElementById("pf-laura");
-// pfPabloBtn.addEventListener("click", PF_P);
-// pfLauraBtn.addEventListener("click", PF_L);
-
-// function PF_P() {
-//     let transferirCantidad = parseFloat(document.getElementById('input-pablo').value);
-//     if (transferirCantidad <= cuentaDePablo.verSaldo()) {
-//         cuentaDePablo.transferencia(transferirCantidad, cuentaDePlazoFijo);
-//         let montoAObtener = transferirCantidad + (transferirCantidad * plazoFijoClassCall.interes);
-//         const divPF = document.createElement("div");
-//         divPF.id = "resume-pf";
-//         divPF.className = "resume-pf";
-//         const h2PF = document.createElement("h2");
-//         h2PF.textContent = "Resumen de Cotización";
-//         divPF.appendChild(h2PF);
-//         // 0
-//         const pf = document.createElement("p");
-//         pf.textContent = "Cuenta " + pfCuentaCliente.nombreCliente;
-//         divPF.appendChild(pf);
-//         // 1
-//         const pf1 = document.createElement("p");
-//         divPF.appendChild(pf1);
-//         pf1.textContent = cuentaDePlazoFijo.banco;
-//         // 2
-//         const pf2 = document.createElement("p");
-//         divPF.appendChild(pf2);
-//         pf2.textContent = "Cuenta número " + cuentaDePlazoFijo.numeroCuenta;
-//         // 3
-//         const pf3 = document.createElement("p");
-//         divPF.appendChild(pf3);
-//         pf3.textContent = "Interés: " + plazoFijoClassCall.interes;
-//         // 4
-//         const pf3_5 = document.createElement("p");
-//         pf3_5.textContent = "Monto a obtener: $" + montoAObtener;
-//         cuentaDePablo.depositoEnCuenta(montoAObtener);
-//         divPF.appendChild(pf3_5);
-//         document.body.appendChild(divPF);
-//         divPF.style.display = 'block';
-//     } else {
-//         alert("Fondos insuficientes.");
-//     }
-//     document.getElementById('input-pablo').value = '';
-//     document.getElementById('input-laura').value = '';
-//     window.setTimeout(function () {
-//         let resumePablo = document.getElementById('resume-pf');
-//         if (resumePablo) {
-//             resumePablo.remove();
-//         }
-//     }, 15000);
-// }
-// function PF_L() {
-//     let transferirCantidad = parseFloat(document.getElementById('input-laura').value);
-//     if (transferirCantidad <= cuentaDeLaura.verSaldo()) {
-//         cuentaDeLaura.transferencia(transferirCantidad, cuentaDePlazoFijo);
-//         let montoAObtener = transferirCantidad + (transferirCantidad * plazoFijoClassCall.interes);
-//         const divPF = document.createElement("div");
-//         divPF.id = "resume-pf-l";
-//         divPF.className = "resume-pf-l";
-//         const h2PF = document.createElement("h2");
-//         h2PF.textContent = "Resumen de Cotización";
-//         divPF.appendChild(h2PF);
-//         // 0
-//         const pf = document.createElement("p");
-//         pf.textContent = "Cuenta " + pfCuentaCliente.nombreCliente;
-//         divPF.appendChild(pf);
-//         // 1
-//         const pf1 = document.createElement("p");
-//         divPF.appendChild(pf1);
-//         pf1.textContent = cuentaDePlazoFijo.banco;
-//         // 2
-//         const pf2 = document.createElement("p");
-//         divPF.appendChild(pf2);
-//         pf2.textContent = "Cuenta número " + cuentaDePlazoFijo.numeroCuenta;
-//         // 3
-//         const pf3 = document.createElement("p");
-//         divPF.appendChild(pf3);
-//         pf3.textContent = "Interés: " + plazoFijoClassCall.interes;
-//         // 4
-//         const pf3_5 = document.createElement("p");
-//         pf3_5.textContent = "Monto a obtener: $" + montoAObtener;
-//         cuentaDeLaura.depositoEnCuenta(montoAObtener);
-//         divPF.appendChild(pf3_5);
-//         document.body.appendChild(divPF);
-//         divPF.style.display = 'block';
-//     } else {
-//         alert("Fondos insuficientes.");
-//     }
-//     window.setTimeout(function () {
-//         let resumeLaura = document.getElementById('resume-pf-l');
-//         if (resumeLaura) {
-//             resumeLaura.remove();
-//         }
-//     }, 15000);
-//     document.getElementById('input-pablo').value = '';
-//     document.getElementById('input-laura').value = '';
-// }
