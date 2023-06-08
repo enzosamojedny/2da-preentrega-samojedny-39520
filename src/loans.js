54613dab1931ace51d926d70fbcc9a0a23add3ff
Enzo Samojedny <enzosamojedny@gmail.com>
2023-06-08 13:55:21 -0300

    form added

--- src/loans.js
+++ src/loans.js
@@ -9,3 +9,65 @@ $(document).ready(function () {
 
     $('.slider-menu').hide(); // Hide all menus by default
 });
+let btn12 = document.getElementById('12')
+let btn24 = document.getElementById('24')
+let btn36 = document.getElementById('36')
+let btn48 = document.getElementById('48')
+
+
+
+btn12.addEventListener('click', () => {
+    const form = document.getElementById('form')
+
+    if (form.style.display === 'none') {
+        form.style.display = 'block';
+    } else {
+        form.style.display = 'none';
+    }
+});
+btn24.addEventListener('click', () => {
+    const form = document.getElementById('form')
+
+    if (form.style.display === 'none') {
+        form.style.display = 'block';
+    } else {
+        form.style.display = 'none';
+    }
+});
+btn36.addEventListener('click', () => {
+    const form = document.getElementById('form')
+
+    if (form.style.display === 'none') {
+        form.style.display = 'block';
+    } else {
+        form.style.display = 'none';
+    }
+});
+btn48.addEventListener('click', () => {
+    const form = document.getElementById('form')
+
+    if (form.style.display === 'none') {
+        form.style.display = 'block';
+    } else {
+        form.style.display = 'none';
+    }
+});
+
+
+
+
+
+
+function firstChoice() {
+    btn12.addEventListener('click')
+}
+
+function secondChoice() {
+
+}
+function thirdChoice() {
+
+}
+function fourthChoice() {
+
+}
