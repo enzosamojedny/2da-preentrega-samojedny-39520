c7ec6c90b633e3df26c73058a574f765c676f077
Enzo Samojedny <enzosamojedny@gmail.com>
2023-06-08 13:10:23 -0300

    loans updates

--- src/loans.js
+++ src/loans.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
-    $('.slider-button').click(function () {
+    $('.loan-btn').click(function () {
+        $(this).parent().parent('.slider-menu').animate({ left: '-100%' }, 300);
+    });
+
+    $('.loan.slider-button').click(function () {
         $(this).next('.slider-menu').slideToggle();
     });
+
+    $('.slider-menu').hide(); // Hide all menus by default
 });
